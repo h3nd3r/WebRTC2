5d24714f5db420fd6e9e0628c5e0e2f9ddfd2b03
Sara Hender <sara@velosmobile.com>
2017-11-27 11:42:10 -0800

    Dump.

---
+++ WebRTC2/FirstViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FirstViewController.swift
+//  WebRTC2
+//
+//  Created by Sara on 11/27/17.
+//  Copyright © 2017 Sara. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class FirstViewController: UIViewController {
+
+  @IBOutlet weak var webView: WKWebView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+
+    webView.load(URLRequest(url: URL(string: "https://appr.tc/r/978949844")!))
+    webView.allowsBackForwardNavigationGestures = true
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+}
+
---
+++ WebRTC2/SecondViewController.swift
@@ -0,0 +1,48 @@
+//
+//  SecondViewController.swift
+//  WebRTC2
+//
+//  Created by Sara on 11/27/17.
+//  Copyright © 2017 Sara. All rights reserved.
+//
+
+import UIKit
+import SafariServices
+
+class SecondViewController: UIViewController, SFSafariViewControllerDelegate {
+
+  var safari: SFSafariViewController?
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+
+    safari = SFSafariViewController(url: URL(string: "https://appr.tc/r/978949844")!)
+    safari?.delegate = self
+    safari?.modalPresentationStyle = .overCurrentContext
+  }
+
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+
+    if let safari = safari {
+      present(safari, animated: false, completion: nil)
+    }
+  }
+
+  func safariViewController(_ controller: SFSafariViewController, didCompleteInitialLoad didLoadSuccessfully: Bool) {
+    //dismiss(animated: false, completion: nil)
+  }
+
+  func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+    self.safari = nil
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
